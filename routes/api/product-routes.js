2eb2499418357ef7d58dc43be1f9f796e6ce0052
matt <mathewandnana@gmail.com>
2023-04-18 14:08:14 -0500

    getting rid of comments from UTA adding info to read.me

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
 
-// get all products
+
+
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  
   Product.findAll({
     attributes: ['id', 'product_name', 'price', 'stock'],
     include: [
@@ -27,10 +26,9 @@ router.get('/', (req, res) => {
     });
 });
 
-// get one product
+
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+ 
   Product.findOne({
     where: {
       id: req.params.id
@@ -60,7 +58,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// create new product
+
 router.post('/', (req, res) => {
  Product.create({
     product_name: req.body.product_name,
@@ -70,7 +68,7 @@ router.post('/', (req, res) => {
     tagIds: req.body.tagIds
     })
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+   
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -80,7 +78,7 @@ router.post('/', (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
+      
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
@@ -90,20 +88,19 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+
 router.put('/:id', (req, res) => {
-  // update product data
+  
   Product.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
     .then((product) => {
-      // find all associated tags from ProductTag
+      
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
     .then((productTags) => {
-      // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
